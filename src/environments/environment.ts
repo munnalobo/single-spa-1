590330e82ec62720e2e6aa38578bd580792447a2
venkata <munnalobo@yahoo.com>
2023-06-28 21:31:36 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "../app/app.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "../app/app-routing.module";
+
+@NgModule()
+export class environment {
+  static production: boolean = false;
+}
